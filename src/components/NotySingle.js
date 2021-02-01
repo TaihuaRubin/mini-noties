84d54fb3b5a8f9688379dc38876e76414780650b
Taihua <Taihua@Taihuas-Macbook-Pro.local>
2021-01-31 23:41:31 -0500

    added archive button and some button styling from bootstrap

--- src/components/NotySingle.js
+++ src/components/NotySingle.js
@@ -1,28 +1,8 @@
 import { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, ButtonGroup, ButtonToolbar } from "react-bootstrap";
 import { connect, useDispatch } from "react-redux";
 import { updateAsRead } from "../redux/notifications";
 import SingleContent from "./SingleContent";
-
-function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal {...props} size='lg' aria-labelledby='contained-modal-title-vcenter' centered>
-      <Modal.Header closeButton>
-        <Modal.Title id='contained-modal-title-vcenter'>Modal heading</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-          Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
 
 function NotySingle(props) {
   // const { title, source, description, publishedAt } = props.notification;
@@ -31,17 +11,20 @@ function NotySingle(props) {
   const dispatch = useDispatch();
   return (
     <div className='noty-single-container'>
-      <h2> Title: {props.notification.title} </h2>
-      <h4> Author: {props.notification.source.name}</h4>
-      <p> Description: {props.notification.description} </p>
-      <p> Published At: {props.notification.publishedAt}</p>
-      <p> Publication: {props.notification.source.url}</p>
-      <button onClick={() => dispatch(updateAsRead(props.notification.title))}> Open to Read More </button>
-      <button variant='primary' onClick={() => setModalShow(true)}>
-        {" "}
-        Open Modal
-      </button>
-      <MyVerticallyCenteredModal show={modalShow} onHide={() => setModalShow(false)} />
+      <h5> {props.notification.title} </h5>
+      <p> Time: {props.notification.publishedAt}</p>
+
+      <ButtonToolbar>
+        <ButtonGroup className='mr-2'>
+          <Button variant='outline-secondary' onClick={() => dispatch(updateAsRead(props.notification.title))}>
+            Archive
+          </Button>
+        </ButtonGroup>
+        <Button variant='outline-info' onClick={() => setModalShow(true)}>
+          Open To Read More
+        </Button>
+      </ButtonToolbar>
+      <SingleContent show={modalShow} onHide={() => setModalShow(false)} content={props.notification} />
     </div>
   );
 }
--- src/components/SingleContent.js
+++ src/components/SingleContent.js
@@ -1,33 +1,27 @@
 import { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Container, Row, Col, Image } from "react-bootstrap";
 import { connect, useDispatch } from "react-redux";
 import { updateAsRead } from "../redux/notifications";
 
 function SingleContent(props) {
-  const dispatch = useDispatch();
-  const [show, setShow] = useState(false);
+  console.log("modal props", props);
+  const { title, description, content, publishedAt, source, ril } = props.content;
+  // const { name } = source; // author
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   return (
-    <div>
-      <Button variant='primary' onClick={handleShow}>
-        Launch static backdrop modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose} backdrop='static' keyboard={false}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>I will not close if you click outside me. Don't even try to press escape key.</Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant='primary'>Understood</Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
+    <Modal {...props} size='lg' aria-labelledby='contained-modal-title-vcenter' centered>
+      <Modal.Header closeButton>
+        <Modal.Title id='contained-modal-title-vcenter'>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>Author: {source["name"]}</p>
+        <p>Publication: {publishedAt}</p>
+        <p>{description}</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
